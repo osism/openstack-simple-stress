f754a6c09b14847f55f23a549b322aa9ad74de1d
Christian Berendt <berendt@betacloud-solutions.de>
2020-03-21 15:00:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# openstack-testsuite
+
+[![Build Status](https://travis-ci.org/betacloud/openstack-testsuite.svg?branch=master)](https://travis-ci.org/betacloud/openstack-testsuite)
+
+## Usage
+
+```
+$ python3 src/instance-spawner.py --parallel 2 --number 2
+2020-03-24 14:05:56 - Checking flavor 1C-1GB-10GB
+2020-03-24 14:06:02 - flavor.id = 30
+2020-03-24 14:06:02 - Checking image Ubuntu 18.04
+2020-03-24 14:06:04 - image.id = f0e2d7ab-5534-40f4-887f-6c9a43a2a043
+2020-03-24 14:06:04 - Checking network net-to-public-testbed
+2020-03-24 14:06:04 - network.id = b11ddb33-a084-4c87-89c7-aa4f9a73b173
+2020-03-24 14:06:04 - Creating server test-0
+2020-03-24 14:06:04 - Creating server test-1
+2020-03-24 14:06:08 - Waiting for server 63bd4f65-9d6b-4d43-b216-515d9e7e5504
+2020-03-24 14:06:08 - Waiting for server 730b941a-2c47-417d-934e-7f0f248ebaf5
+2020-03-24 14:06:51 - Waiting for running tests on 63bd4f65-9d6b-4d43-b216-515d9e7e5504
+2020-03-24 14:06:54 - Waiting for running tests on 730b941a-2c47-417d-934e-7f0f248ebaf5
+2020-03-24 14:08:28 - Deleting server 63bd4f65-9d6b-4d43-b216-515d9e7e5504
+2020-03-24 14:08:29 - Server 63bd4f65-9d6b-4d43-b216-515d9e7e5504 finished
+2020-03-24 14:08:30 - Deleting server 730b941a-2c47-417d-934e-7f0f248ebaf5
+2020-03-24 14:08:32 - Server 730b941a-2c47-417d-934e-7f0f248ebaf5 finished
+```
---
+++ src/instance-spawner.py
@@ -0,0 +1,82 @@
+import base64
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import logging
+import sys
+import time
+
+import openstack
+from oslo_config import cfg
+
+PROJECT_NAME = "openstack-instance-spawner"
+CONF = cfg.CONF
+opts = [
+    cfg.BoolOpt('floating', default=False),
+    cfg.IntOpt('number', default=1),
+    cfg.IntOpt('parallel', default=1),
+    cfg.StrOpt('cloud', help='Cloud name in clouds.yaml', default='testbed'),
+    cfg.StrOpt('flavor', default='1C-1GB-10GB'),
+    cfg.StrOpt('image', default='Ubuntu 18.04'),
+    cfg.StrOpt('keypair'),
+    cfg.StrOpt('network', default='net-to-public-testbed'),
+    cfg.StrOpt('prefix', default='test')
+]
+
+CONF.register_cli_opts(opts)
+CONF(sys.argv[1:], project=PROJECT_NAME)
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+
+def run(x, image, flavor, network, user_data):
+    name = "%s-%d" % (CONF.prefix, x)
+    logging.info("Creating server %s" % name)
+    server = cloud.compute.create_server(
+        name=name, image_id=image.id, flavor_id=flavor.id,
+        networks=[{"uuid": network.id}], user_data=b64_user_data)
+
+    logging.info("Waiting for server %s" % server.id)
+    server = cloud.compute.wait_for_server(server)
+
+    logging.info("Waiting for running tests on %s" % server.id)
+    while True:
+        time.sleep(5.0)
+        console = cloud.compute.get_server_console_output(server)
+        if "DONE DONE DONE" in str(console):
+            break
+
+    logging.info("Deleting server %s" % server.id)
+    cloud.compute.delete_server(server)
+
+    return server.id
+
+
+cloud = openstack.connect(cloud=CONF.cloud)
+
+user_data = """
+#cloud-config
+runcmd:
+ - [ sh, -xc, "dd if=/dev/zero of=/tmp/laptop.bin bs=128M count=8 oflag=direct" ]
+ - [ sh, -xc, "sleep 10" ]
+ - [ sh, -xc, "echo $(date) ': DONE DONE DONE'" ]
+"""
+b64_user_data = base64.b64encode(user_data.encode('utf-8')).decode('utf-8')
+
+logging.info("Checking flavor %s" % CONF.flavor)
+flavor = cloud.get_flavor(CONF.flavor)
+logging.info("flavor.id = %s" % flavor.id)
+
+logging.info("Checking image %s" % CONF.image)
+image = cloud.get_image(CONF.image)
+logging.info("image.id = %s" % image.id)
+
+logging.info("Checking network %s" % CONF.network)
+network = cloud.get_network(CONF.network)
+logging.info("network.id = %s" % network.id)
+
+pool = ThreadPoolExecutor(max_workers=CONF.parallel)
+futures = []
+for x in range(CONF.number):
+    futures.append(pool.submit(run, x, image, flavor, network, user_data))
+
+for x in as_completed(futures):
+    logging.info("Server %s finished" % x.result())
